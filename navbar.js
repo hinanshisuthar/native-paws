ce4af41733e842a0f4831a1681431b72079af2dd
Hinanshi Suthar <hinanshis4@gmail.com>
2022-02-19 15:25:24 +0530

    feat: nav menu

--- navbar.js
+++ navbar.js
@@ -6,3 +6,16 @@ function openMenu() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+const openHam = document.querySelector(".menu")
+const hamIcon = document.querySelector(".bars-con")
+
+hamIcon.addEventListener("click", menu);
+
+    function menu() {
+        console.log("here");
+        if(openHam.style.display === "block") {
+            openHam.style.display = "none";
+        } else {
+        openHam.style.display = "block";
+        }
+    }
